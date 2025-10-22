58714b936683acc9141496d2dfc790b964bbc0f5
Sergio33499 <sergiopuig@iesbetxi.es>
2025-10-22 13:39:18 +0200

    Ejercicio de combate empezado

--- src/Tema2/EjercicioCombate.java
+++ src/Tema2/EjercicioCombate.java
@@ -15,6 +15,7 @@ public class EjercicioCombate {
 
        System.out.println("----->EMPIEZA EL COMBATE<-----");
 
+       //---JUGADOR 1---
        while (!valido1){
            System.out.println("Introduce los datos del Jugador1 (1-200)");
            System.out.println("Introduce la velocidad: ");
@@ -26,12 +27,53 @@ public class EjercicioCombate {
            System.out.println("Introduce la vida: ");
            vidaJ1 = scanner.nextInt();
 
+           suma1 = velocidadJ1 + ataqueJ1 + defensaJ1 + vidaJ1;
 
+           if (velocidadJ1 < 1 || ataqueJ1 < 1 || defensaJ1 < 1 || vidaJ1 < 1 || velocidadJ1 > 200 || ataqueJ1 > 200 || defensaJ1 > 200 ||vidaJ1 > 200){
+               System.out.println("ERROR: Los atributos tienen que estar entre 1-200");
+           } else if (suma1 > 500) {
+               System.out.println("ERROR: La suma de los atributos no puede superar 500");
+           }else {
+               valido1 = true;
+           }
+       }
 
+       //---JUGADOR 2---
+       while (!valido2){
+           System.out.println("Introduce los datos del Jugador2 (1-200)");
+           System.out.println("Introduce la velocidad: ");
+           velocidadJ2 = scanner.nextInt();
+           System.out.println("Introduce el ataque: ");
+           ataqueJ2 = scanner.nextInt();
+           System.out.println("Introduce la defensa: ");
+           defensaJ2 = scanner.nextInt();
+           System.out.println("Introduce la vida: ");
+           vidaJ2 = scanner.nextInt();
 
+           suma2 = velocidadJ2 + ataqueJ2 + defensaJ2 + vidaJ2;
+
+           if (velocidadJ2 < 1 || ataqueJ2 < 1|| defensaJ2 < 1 || vidaJ2 < 1 || velocidadJ2 > 200 || ataqueJ2 > 200 || defensaJ2 > 200 || vidaJ2 > 200){
+               System.out.println("ERROR: Los atributos tienen que estar entre 1-200");
+           } else if (suma2 > 500) {
+               System.out.println("ERROR: La suma de los atributos no puede superar 500");
+           }else {
+               valido2 = true;
+           }
+       }
+
+       //---COMBATE---
+       System.out.println("¡COMIENZA EL COMBATE!");
+       int turno = 1;
+
+       while (vidaJ1 > 0 && vidaJ2 > 0){
+           System.out.println();
        }
 
 
 
+
+
+
+
    }
-}//a
+}
