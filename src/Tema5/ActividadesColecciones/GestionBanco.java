4d23cd8a246fc21add6604184b3d072617f31408
Sergio33499 <sergiopuig@iesbetxi.es>
2026-02-05 12:16:17 +0100

    Ejercicio 3 Terminado

--- src/Tema5/ActividadesColecciones/GestionBanco.java
+++ src/Tema5/ActividadesColecciones/GestionBanco.java
@@ -2,6 +2,7 @@ package Tema5.ActividadesColecciones;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GestionBanco {
     List<Usuario> cola = new LinkedList<>();
@@ -10,7 +11,22 @@ public class GestionBanco {
         cola.add(u);
     }
 
+    public void quitarPrimeroDeLaCola(){
+        if (cola.isEmpty()){
+            System.out.println("No se puede quitar a alguien de la cola porque esta vacia");
+        }else {
+            cola.remove(0);
+            System.out.println("El primer usuario ha sido atendido.");
+        }
+    }
 
+    public void quitarDeCualquierPosicion(int posicion){
+        if (posicion >= 0 && posicion < cola.size()){
+            cola.remove(posicion);
+        }else {
+            System.out.println("Esa posicion no existe en la cola!");
+        }
+    }
 
     public class Usuario{
         String nombre;
@@ -49,4 +65,45 @@ public class GestionBanco {
             return edad;
         }
     }
+
+    public static void main(String[] args) {
+        GestionBanco banco = new GestionBanco();
+        Scanner scanner = new Scanner(System.in);
+        int opcion = -1;
+
+        while (opcion != 0){
+            System.out.println("\n--- GESTIÓN DEL BANCO ---");
+            System.out.println("1. Añadir cliente");
+            System.out.println("2. Atender al primero");
+            System.out.println("3. Cliente se cansa (quitar por posición)");
+            System.out.println("0. Salir");
+            opcion = scanner.nextInt();
+
+            switch(opcion) {
+                case 1:
+                    scanner.nextLine();
+                    System.out.println("Introduce el nombre: ");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Introduce el DNI: ");
+                    String dni = scanner.nextLine();
+                    System.out.println("Introduce la edad: ");
+                    int edad = scanner.nextInt();
+                    Usuario newUser = banco.new Usuario(nombre,dni,edad);
+                    banco.anadirALaCola(newUser);
+                    System.out.println("Usuario añadido correctamente!");
+                    break;
+
+                case 2:
+                    banco.quitarPrimeroDeLaCola();
+                    break;
+
+                case 3:
+                    System.out.println("Introduce la posicion a eliminar:");
+                    int pos = scanner.nextInt();
+                    banco.quitarDeCualquierPosicion(pos);
+                    break;
+            }
+        }
+    }
+
 }
---
+++ src/Tema5/ActividadesColecciones/ListaCompraAmpliado.java
@@ -0,0 +1,5 @@
+package Tema5.ActividadesColecciones;
+
+public class ListaCompraAmpliado {
+
+}
