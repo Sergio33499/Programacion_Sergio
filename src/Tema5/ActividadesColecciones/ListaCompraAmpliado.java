543b963741f2b6691eb38845592ff9487e25fd4a
Sergio33499 <sergiopuig@iesbetxi.es>
2026-02-08 14:27:35 +0100

    Ejercicio ListaCompraAmpliado por hacer

--- src/Tema5/ActividadesColecciones/ListaCompraAmpliado.java
+++ src/Tema5/ActividadesColecciones/ListaCompraAmpliado.java
@@ -1,5 +1,15 @@
 package Tema5.ActividadesColecciones;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class ListaCompraAmpliado {
 
+    Set<String> products = new HashSet<>();
+    Set<String> carroCompra = new HashSet<>();
+
+    public static void main(String[]args){
+
+    }
 }
