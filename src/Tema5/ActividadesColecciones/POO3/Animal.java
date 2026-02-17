41b47591a85f92d591231c36584f4b5a3f9787f2
Sergio33499 <sergiopuig@iesbetxi.es>
2026-02-17 18:36:16 +0100

    Ejercicios POO3 (Ej4 sin terminar)

--- src/Tema5/ActividadesColecciones/POO3/Animal.java
+++ src/Tema5/ActividadesColecciones/POO3/Animal.java
@@ -1,5 +1,131 @@
 package Tema5.ActividadesColecciones.POO3;
 
-public class Animal {
+abstract class Animal {
+    String nombre;
+    int edad;
 
+    abstract void comer();
+
+    public void dormir() {
+        System.out.println("Cierran los ojos y descansan.");
+    }
+
+    abstract void hacerRuido();
+
+    public Animal(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+class Perro extends Animal{
+
+    @Override
+    void comer() {
+        System.out.println("Come pienso.");
+    }
+
+    @Override
+    void hacerRuido() {
+        System.out.println("Guau Guau!");
+    }
+
+
+
+    public Perro(String nombre, int edad){
+        super(nombre,edad);
+    }
+
+    public void traerPelota(){
+        System.out.println("El perro ha traido la pelota!");
+    }
+}
+
+class Gato extends Animal{
+
+    @Override
+    void comer() {
+        System.out.println("Come latas de atún.");
+    }
+
+    @Override
+    void hacerRuido() {
+        System.out.println("Miau Miau!");
+    }
+
+    public Gato(String nombre, int edad){
+        super(nombre, edad);
+    }
+
+    public void escupirBolaPelo(){
+        System.out.println("El gato escupió una bola de pelo");
+    }
+}
+
+class Leon extends Animal{
+
+    @Override
+    void comer() {
+        System.out.println("Come animales.");
+    }
+
+    @Override
+    void hacerRuido() {
+        System.out.println("Grrr Grrr!");
+    }
+
+    public Leon(String nombre, int edad){
+        super(nombre, edad);
+    }
+
+    public void cazar(){
+        System.out.println("El león ha cazado una zebra.");
+    }
+}
+
+class Pajaro extends Animal{
+
+    @Override
+    void comer() {
+        System.out.println("Come semillas.");
+    }
+
+    @Override
+    void hacerRuido() {
+        System.out.println("Pio Pio!");
+    }
+
+    public Pajaro(String nombre, int edad){
+        super(nombre, edad);
+    }
+
+    public void volar(){
+        System.out.println("El pájaro se fué volando!");
+    }
+}
+
+class Pez extends Animal{
+
+    public Pez(String nombre, int edad){
+        super(nombre, edad);
+    }
+
+    @Override
+    void comer() {
+        System.out.println("Come plancton.");
+    }
+
+    @Override
+    public void dormir(){
+        System.out.println("Flota y descansa.");
+    }
+
+    @Override
+    void hacerRuido() {
+        System.out.println("Glu Glu!");
+    }
+
+    public void nadar(){
+        System.out.println("El pez se fué nadando!");
+    }
 }
---
+++ src/Tema5/ActividadesColecciones/POO3/Ficha.java
@@ -0,0 +1,58 @@
+package Tema5.ActividadesColecciones.POO3;
+
+public class Ficha {
+    int numero;
+    String titulo;
+    boolean prestado = false;
+
+    public Ficha(int numero, String titulo) {
+        this.numero = numero;
+        this.titulo = titulo;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Numero del ejemplar: " + numero + " y titulo: " + titulo);
+    }
+
+    public void prestar() {
+        if (this.prestado) {
+            System.out.println("El ejemplar esta prestado.");
+        } else {
+            this.prestado = true;
+            System.out.println("El ejemplar ha sido prestado.");
+        }
+    }
+
+    public void devolver() {
+        if (!this.prestado) {
+            System.out.println("El ejemplar esta disponible en la biblioteca.");
+        } else {
+            this.prestado = false;
+            System.out.println("Se ha procesado la devolucion");
+        }
+    }
+
+    public String estadoActual() {
+        if (this.prestado) {
+            return "Estado: PRESTADO";
+        } else {
+            return "Estado: DISPONIBLE";
+        }
+    }
+}
+
+class Libro extends Ficha{
+    String autor;
+    String editorial;
+
+    public Libro(int numero, String titulo, String autor, String editorial){
+        super(numero, titulo);
+        this.autor = autor;
+        this.editorial = editorial;
+    }
+
+    public String toString(){
+        return "Libro: " + titulo + ", Autor: " + autor + ", Editorial" + editorial;
+    }
+
+}
